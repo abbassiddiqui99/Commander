34e5ca7ae5ae6f3f704848e1fe5fd245180847dc
Abbas Hussain <abbassiddiqui99@gmail.com>
2021-05-21 07:28:21 +0500

    part 2 completed

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,8 @@ namespace Commander
                 x => x.UseSqlServer(Configuration.GetConnectionString("CommanderConnection"))
             );
             services.AddControllers();
-            services.AddScoped<ICommanderRepo,CommanderRepo>();
+            // services.AddScoped<ICommanderRepo,CommanderRepo>();
+            services.AddScoped<ICommanderRepo,SqlCommanderRepo>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Commander", Version = "v1" });
